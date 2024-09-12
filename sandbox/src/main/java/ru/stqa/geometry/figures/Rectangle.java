5fa42d9364b248702bc7eb341b5c60b55b11b2a2
AAPopova94 <a.a.popova94@gmail.com>
2024-09-12 14:30:20 +0300

    добавление record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,7 @@
 package ru.stqa.geometry.figures;
 
-public class Rectangle {
+public record Rectangle (double a, double b) // если ставить record вместо class, можно сократить свойство
+{
     public static void printRectagleArea(double a, double b) {
         var text = String.format("Площадь прямоугольника со сторонами %f + %f = %f", a, b, rectagleArea(a, b));
         System.out.println(text);
