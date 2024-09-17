d60abbb8f6ba8d2d069caa709b47ad53ee8e6aa5
AAPopova94 <a.a.popova94@gmail.com>
2024-09-17 11:19:24 +0300

    Залание 4, выполнено, вопросы отпали

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -41,7 +41,10 @@ public record Triangle (double sideA, double sideB, double sideC) {
         Triangle triangle = (Triangle) o;
         return (Double.compare(this.sideA, triangle.sideA) == 0 && Double.compare(this.sideB, triangle.sideB) == 0 && Double.compare(this.sideC, triangle.sideC) == 0)
                 || (Double.compare(this.sideB, triangle.sideA) == 0 && Double.compare(this.sideC, triangle.sideB) == 0 && Double.compare(this.sideA, triangle.sideC) == 0)
-                || (Double.compare(this.sideC, triangle.sideA) == 0 && Double.compare(this.sideA, triangle.sideB) == 0 && Double.compare(this.sideB, triangle.sideC) == 0);
+                || (Double.compare(this.sideC, triangle.sideA) == 0 && Double.compare(this.sideA, triangle.sideB) == 0 && Double.compare(this.sideB, triangle.sideC) == 0)
+                ||(Double.compare(this.sideA, triangle.sideA) == 0 && Double.compare(this.sideC, triangle.sideB) == 0 && Double.compare(this.sideB, triangle.sideC) == 0)
+                || (Double.compare(this.sideC, triangle.sideA) == 0 && Double.compare(this.sideB, triangle.sideB) == 0 && Double.compare(this.sideA, triangle.sideC) == 0)
+                || (Double.compare(this.sideB, triangle.sideA) == 0 && Double.compare(this.sideA, triangle.sideB) == 0 && Double.compare(this.sideC, triangle.sideC) == 0);
     }
 
     @Override
