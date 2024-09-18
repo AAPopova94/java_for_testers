f6c3be94ef8ca4e76ce445e509b7b952b1e0a077
AAPopova94 <a.a.popova94@gmail.com>
2024-09-18 15:38:41 +0300

    как будто сделано, но есть ошибка и я не знаю какая

--- addressbock/src/test/java/DeletegGroupTests.java
+++ addressbock/src/test/java/DeletegGroupTests.java
@@ -49,6 +49,16 @@ public class DeletegGroupTests {
     if (!isElementPresent(By.name("new"))) {
       driver.findElement(By.linkText("groups")).click();
     }
+    if (!isElementPresent(By.linkText("selected[]"))) {
+      driver.findElement(By.name("new")).click();
+      driver.findElement(By.name("group_name")).click();
+      driver.findElement(By.name("group_name")).sendKeys("groupname");
+      driver.findElement(By.name("group_header")).click();
+      driver.findElement(By.name("group_header")).sendKeys("1");
+      driver.findElement(By.name("group_footer")).click();
+      driver.findElement(By.name("group_footer")).sendKeys("1");
+      driver.findElement(By.name("submit")).click();
+    }
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("selected[]")).click();
     driver.findElement(By.name("delete")).click();
